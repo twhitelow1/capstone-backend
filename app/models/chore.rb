3dca1ea8a8dc29ea7a45a247ddf8384bc0b7cdfb
Todd Whitelow <todd@whitelow.tech>
2020-11-03 01:50:33 -0700

    add associations

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,4 @@
 class Chore < ApplicationRecord
+  belongs_to :room
+  has_many :users, through: :assignments
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  belongs_to :home
+  has_many :chores
 end
