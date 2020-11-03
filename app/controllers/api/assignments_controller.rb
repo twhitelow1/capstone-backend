1aa0deaeeb84bf532befed83e816156d0d3f2add
Todd Whitelow <todd@whitelow.tech>
2020-11-03 02:09:31 -0700

    add assignment routes, actions, views

--- app/controllers/api/assignments_controller.rb
+++ app/controllers/api/assignments_controller.rb
@@ -1,2 +1,41 @@
 class Api::AssignmentsController < ApplicationController
+  def index
+    @assignments = Assignment.all
+    render "index.json.jb"
+  end
+
+  def create
+    assignment = Assignment.new(
+      chore_id: params[:chore_id],
+      user_id: params[:user_id],
+      due_date: params[:due_date],
+      completed_date: params[:completed_date],
+      assigner_id: params[:assigner_id],
+    )
+    if assignment.save
+      render json: { message: "Assignment created successfully" }, status: :created
+    else
+      render json: { errors: assignment.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    @assignment = Assignment.find_by(id: params[:id])
+    @assignment.chore_id = params[:chore_id] || @assignment.chore_id
+    @assignment.user_id = params[:user_id] || @assignment.user_id
+    @assignment.due_date = params[:due_date] || @assignment.due_date
+    @assignment.completed_date = params[:completed_date] || @assignment.completed_date
+    @assignment.assigner_id = params[:assigner_id] || @assignment.assigner_id
+    if @assignment.save
+      render json: {message:"Assignment updated successfully"}, status: :created
+    else
+      render json: {errors: @assignment.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    @assignment = Assignment.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+
+    get "/chores" => "chores#index"
+
+    get "/assignments" => "assignments#index"
+    get "/assignments/:id" => "assignments#show"
+    post "/assignments" => "assignments#create"
+    put "/assignments/:id" => "assignments#update"
   end
 end
