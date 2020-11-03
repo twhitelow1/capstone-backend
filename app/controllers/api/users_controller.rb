5c29d7ceb56a387224abc1637eaab811bb8f5863
Todd Whitelow <todd@whitelow.tech>
2020-11-03 01:47:47 -0700

    add authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,21 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    user = User.new(
+      email: params[:email],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      house_id: params[:house_id],
+      points: params[:points],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
-  post "/users" => "users#create"
-
+  namespace :api do
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
+  end
 end
