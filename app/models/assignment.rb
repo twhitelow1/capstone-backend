33f94901f38a05b485681160fd5f5d8e31f4c298
Todd Whitelow <todd@whitelow.tech>
2020-11-04 15:18:31 -0700

    update assignments model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,9 +2,7 @@ class Assignment < ApplicationRecord
   belongs_to :user  
   belongs_to :chore
 
-  validates :due_date, presence: true
   validates :assigner_id, numericality: { greater_than: 0 }
-  validates :user_id, numericality: { greater_than: 0 }
 
   scope :show_completed, ->(completed_status) { where(completed: true) if completed_status == "true"}
 
