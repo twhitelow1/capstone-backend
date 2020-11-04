4e74e711dd870ef78754c6043ca6be62a4f1c1ba
Todd Whitelow <todd@whitelow.tech>
2020-11-04 05:34:59 -0700

    stack the order on the where in the index

--- app/controllers/api/assignments_controller.rb
+++ app/controllers/api/assignments_controller.rb
@@ -1,11 +1,10 @@
 class Api::AssignmentsController < ApplicationController
   def index
-    @assignments = Assignment.where(completed: false)
+    @assignments = Assignment.where(completed: false).order(due_date: :asc)
     if params[:completed] == "true"
       @assignments = Assignment.where(completed: true)
     end
     # @assignments = Assignment.show_completed(params[:completed])
-    @assignments = @assignments.order(due_date: :asc)
     render "index.json.jb"
   end
 
