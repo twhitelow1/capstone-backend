6916c0b81d42ac0be9254568bef4eabc080dfbdc
Todd Whitelow <todd@whitelow.tech>
2020-11-04 06:30:16 -0700

    add assigner method to assignmet class. it returns the assigner's user hash

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,8 +2,14 @@ class Assignment < ApplicationRecord
   belongs_to :user  
   belongs_to :chore
 
-  
+  validates :due_date, presence: true
+  validates :assigner_id, numericality: { greater_than: 0 }
+  validates :user_id, numericality: { greater_than: 0 }
 
   scope :show_completed, ->(completed_status) { where(completed: true) if completed_status == "true"}
+
+  def assigner
+    return User.find_by(id: id)
+  end
 
 end
