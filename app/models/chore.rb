34fa6033a12f327be2f11cb4ec4ddbe36a69b375
Todd Whitelow <todd@whitelow.tech>
2020-11-10 10:49:19 -0700

    add chore.assigned

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,7 +2,12 @@ class Chore < ApplicationRecord
   belongs_to :room
   has_many :users, through: :assignments
 
-  def users
+  def assigned
+    result = Assignment.where(chore_id: id).map{ |assignment| !assignment.completed}
+    return result.length > 0 
+  end
+
+  def household
     return User.where(home_id: room.home_id)
   end
 end
