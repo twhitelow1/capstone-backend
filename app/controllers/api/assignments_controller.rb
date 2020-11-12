947821053a24daf2ea79655cbb33c36038f89851
Todd Whitelow <todd@whitelow.tech>
2020-11-12 01:04:02 -0700

    update items complete status

--- app/controllers/api/assignments_controller.rb
+++ app/controllers/api/assignments_controller.rb
@@ -25,18 +25,25 @@ class Api::AssignmentsController < ApplicationController
     end
   end
 
+  
+
   def update
-    @assignment = Assignment.find_by(id: params[:id])
-    @assignment.chore_id = params[:chore_id] || @assignment.chore_id
-    @assignment.user_id = params[:user_id] || @assignment.user_id
-    @assignment.due_date = params[:due_date] || @assignment.due_date
-    @assignment.completed_date = params[:completed_date] || @assignment.completed_date
-    @assignment.completed = params[:completed] || @assignment.completed
-    @assignment.assigner_id = params[:assigner_id] || @assignment.assigner_id
-    if @assignment.save
+    if params[:id] == "completed"
+      Assignment.where(:id => params[:assignments]).update(:completed => true, :completed_date => DateTime.now())
       render json: {message:"Assignment updated successfully"}, status: :created
     else
-      render json: {errors: @assignment.errors.full_messages }, status: :bad_request
+      @assignment = Assignment.find_by(id: params[:id])
+      @assignment.chore_id = params[:chore_id] || @assignment.chore_id
+      @assignment.user_id = params[:user_id] || @assignment.user_id
+      @assignment.due_date = params[:due_date] || @assignment.due_date
+      @assignment.completed_date = params[:completed_date] || @assignment.completed_date
+      @assignment.completed = params[:completed] || @assignment.completed
+      @assignment.assigner_id = params[:assigner_id] || @assignment.assigner_id
+      if @assignment.save
+        render json: {message:"Assignment updated successfully"}, status: :created
+      else
+        render json: {errors: @assignment.errors.full_messages }, status: :bad_request
+      end
     end
   end
 
